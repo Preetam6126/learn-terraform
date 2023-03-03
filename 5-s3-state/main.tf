3b6b866eb307be027c9c10c0d244e60e25983d3e
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-03-03 17:22:06 +0000

    good

---
+++ 5-s3-state/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-71b"
+    key    = "5-s3-state/infra.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
+resource "aws_instance" "ec2" {
+    ami ="ami-0a017d8ceb274537d"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = ["sg-0dfcc82daa20fc54d"]
+    tags = {
+        Name = "test"
+    }
+}

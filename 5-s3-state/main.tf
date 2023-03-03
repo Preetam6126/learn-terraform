f766ffbe9728b9096244a970cdba0e1b8a770bce
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-03-03 17:28:14 +0000

    good

--- 5-s3-state/main.tf
+++ 5-s3-state/main.tf
@@ -12,6 +12,6 @@ resource "aws_instance" "ec2" {
     instance_type = "t3.micro"
     vpc_security_group_ids = ["sg-0dfcc82daa20fc54d"]
     tags = {
-        Name = "test"
+        Name = "demo"
     }
 }

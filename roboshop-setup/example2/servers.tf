bed733e0e57ffea7f926209fd7e00850baaaa57c
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-03-04 23:51:09 +0000

    good

--- roboshop-setup/example2/servers.tf
+++ roboshop-setup/example2/servers.tf
@@ -1,8 +1,9 @@
-data "aws_ami" "example" 
+data "aws_ami" "example" {
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
   owners           = ["973714476881"]
 }
+
 
 resource "aws_instance" "frontend" {
     count = length(var.instances)

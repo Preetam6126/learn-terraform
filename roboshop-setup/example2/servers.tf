52ada6defe32bb4ff521d9f942457805550d7e5e
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-03-04 23:48:23 +0000

    good

--- roboshop-setup/example2/servers.tf
+++ roboshop-setup/example2/servers.tf
@@ -1,4 +1,4 @@
-data "aws_ami" "example" {
+data "aws_ami" "example" 
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
   owners           = ["973714476881"]
@@ -9,8 +9,11 @@ resource "aws_instance" "frontend" {
     ami = data.aws_ami.example.image_id
     instance_type = "t3.micro"
     vpc_security_group_ids = ["sg-0dfcc82daa20fc54d"]
-   
+    tags = {
+        Name = var.instances[count.index]
     }
+   
+}
 
 variable "instances" {
   

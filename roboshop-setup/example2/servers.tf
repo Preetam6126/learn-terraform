532ffde873440280bd3dd9c26b6795618f68d8d0
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-03-04 23:31:18 +0000

    good

--- roboshop-setup/example2/servers.tf
+++ roboshop-setup/example2/servers.tf
@@ -1,23 +1,43 @@
-data "aws_ami" "example" {
-  most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["973714476881"]
-}
+# data "aws_ami" "example" {
+#   most_recent      = true
+#   name_regex       = "Centos-8-DevOps-Practice"
+#   owners           = ["973714476881"]
+# }
 
-resource "aws_instance" "frontend" {
-    count = lenght(var.instances)
-    ami = data.aws_ami.example.image_id
-    instance_type = "t3.micro"
-    vpc_security_group_ids = ["sg-0dfcc82daa20fc54d"]
+# resource "aws_instance" "frontend" {
+#     count = lenght(var.instances)
+#     ami = data.aws_ami.example.image_id
+#     instance_type = "t3.micro"
+#     vpc_security_group_ids = ["sg-0dfcc82daa20fc54d"]
    
-    }
+#     }
 
-variable "instances" {
+# variable "instances" {
   
-   default = ["cart", "catalogue", "user", "payment", "shipping"]
+#   default = ["cart", "catalogue", "user", "payment", "shipping"]
+# }
+
+# output "public-ip" {
+    
+#     value = aws_instance.frontend.*.public_ip
+# }
+
+variable "d1" {
+    
+    default = [
+    {
+        course_name = "devops"
+        trainer_name = "ravi"
+    }
+    {
+        course_name = "aws"
+        trainer_name = "raju"
+    }        
+    ]
 }
 
-output "public-ip" {
+output "course" {
     
-    value = aws_instance.frontend.*.public_ip
+    value = var.d1.*.course_name
+    
 }

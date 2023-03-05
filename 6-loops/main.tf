93cdbe656c62bdca76634ec08040cd17996245cf
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-03-05 00:38:37 +0000

    good

--- 6-loops/main.tf
+++ 6-loops/main.tf
@@ -7,3 +7,35 @@ resource "null_resource" "nothing" {
     
     count = var.d1
 }
+
+variable "d2" {
+    
+    default = [ "guava", "cherry", "strawberry"]
+}
+
+resource "null_resource" "d2" {
+    
+    count = length(var.d2)
+}
+
+variable "d3" {
+    
+    default = {
+        
+        fruit = {
+            name = "apple"
+            count = 3
+        }
+        
+        vegetalbe = {
+            name = "lemon"
+            count = 5
+        }
+    }
+}
+
+
+resource "null_resource" "d3" {
+    
+    for_each = var.d3
+}

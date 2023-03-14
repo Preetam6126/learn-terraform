a3f178af8a1921326e6e7bbcbba5dece5b36ca33
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-03-14 01:25:30 +0000

    good

--- 5-s3-state/main.tf
+++ 5-s3-state/main.tf
@@ -7,11 +7,18 @@ terraform {
 }
 
 
-resource "aws_instance" "ec2" {
-    ami ="ami-0a017d8ceb274537d"
-    instance_type = "t3.micro"
-    vpc_security_group_ids = ["sg-0dfcc82daa20fc54d"]
-    tags = {
-        Name = "demo"
+# resource "aws_instance" "ec2" {
+#     ami ="ami-0a017d8ceb274537d"
+#     instance_type = "t3.micro"
+#     vpc_security_group_ids = ["sg-0dfcc82daa20fc54d"]
+#     tags = {
+#         Name = "demo"
+#     }
+# }
+
+
+resource "null_resource" "provisioner" {
+  provisioner "remote-exec" {
+        
+        
     }
-}

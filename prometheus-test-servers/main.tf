925d4c295cdbd5d7a3ebfa632865adf48dba2b1e
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-03-19 22:34:52 +0000

    good

--- prometheus-test-servers/main.tf
+++ prometheus-test-servers/main.tf
@@ -2,7 +2,8 @@ resource "aws_spot_instance_request" "node1" {
   ami           = "ami-0bd9a7e76c2ce901a"
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-0dfcc82daa20fc54d"]
-  wait_for_fullfillment = true
+  wait_for_fulfillment = true
+  
 
   tags = {
     Name = "prom-test-server"
@@ -13,7 +14,7 @@ resource "aws_spot_instance_request" "node2" {
   ami           = "ami-0bd9a7e76c2ce901a"
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-0dfcc82daa20fc54d"]
-  wait_for_fullfillment = true
+  wait_for_fulfillment = true
 
   tags = {
     Name = "prom-test-node"

d391b34dcb0bfc397601cd0bbd62ba42012a3423
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-03-19 22:43:06 +0000

    good

--- prometheus-test-servers/main.tf
+++ prometheus-test-servers/main.tf
@@ -4,10 +4,15 @@ resource "aws_spot_instance_request" "node1" {
   vpc_security_group_ids = ["sg-0dfcc82daa20fc54d"]
   wait_for_fulfillment = true
   
-
   tags = {
     Name = "prom-test-server"
   }
+}
+
+resource "aws_ec2_tag" "node1" {
+  resource_id = aws_spot_instance_request.node1.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-server"
 }
 
 resource "aws_spot_instance_request" "node2" {
@@ -19,4 +24,10 @@ resource "aws_spot_instance_request" "node2" {
   tags = {
     Name = "prom-test-node"
   }
+}
+
+resource "aws_ec2_tag" "node2" {
+  resource_id = aws_spot_instance_request.node1.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-node"
 }

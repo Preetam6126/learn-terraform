41a93cf1f467617c18c79e22c3d86fc89ec03023
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-05-05 15:42:28 +0000

    testing

--- 3-data/data.tf
+++ 3-data/data.tf
@@ -1,10 +1,16 @@
-data "aws_ami" "example" {
-  most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["973714476881"]
-}
+# data "aws_ami" "example" {
+#   most_recent      = true
+#   name_regex       = "Centos-8-DevOps-Practice"
+#   owners           = ["973714476881"]
+# }
 
-output "ami-id" {
-value = data.aws_ami.example.image_id
+# output "ami-id" {
+# value = data.aws_ami.example.image_id
     
+# }
+
+
+data "aws_instance" "jenkins" {
+  
+  instance_id = "i-0ceae52abd7e93392"
 }
